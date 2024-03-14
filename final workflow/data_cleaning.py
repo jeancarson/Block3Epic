ba900c6d19b6df6e2ecae2ead2f7c0153f3b0a32
jeancarson <jeanloucarson@gmail.com>
2024-03-14 16:18:12 +0000

    basic visualisaton added, everyone add their own

---
+++ final workflow/data_cleaning.py
@@ -0,0 +1,3 @@
+#This is a .py file so it can be run in the model notebooks
+#My idea was to have one hot encoding done here, then removal of any outliers, bsaed on judgments made in the data visualisation file
+#Idk if this is best, but then we are all working off one cleaned dataset
